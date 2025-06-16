1bca70ec800bb42f85a25f21cbb54f5c545d0ec4
AbdulHadi <AH1920081908@gmail.com>
2025-06-16 12:01:09 +0500

    chapter 03
    javascript assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+// 1. Declare a variable age and show it in alert
+var age = 17;
+alert("I am " + age + " years old");
+
+// 2. Track number of visits (using localStorage to persist data)
+var visitCount = localStorage.getItem("visitCount");
+
+if (visitCount === null) {
+    visitCount = 1;
+} else {
+    visitCount = Number(visitCount) + 1;
+}
+
+localStorage.setItem("visitCount", visitCount);
+document.write("You have visited this site " + visitCount + " times<br>");
+
+// 3. Declare birth year and show it
+var birthYear = 2008;
+document.write("My birth year is " + birthYear + "<br>");
+document.write("Data type of my declared variable is " + typeof (birthYear) + "<br>");
+
+// 4. Clothing store order message
+var visitorName = "John Doe";
+var productTitle = "T-shirt(s)";
+var quantity = 5;
+
+document.write(visitorName + " ordered " + quantity + " " + productTitle + " on XYZ Clothing store.");
